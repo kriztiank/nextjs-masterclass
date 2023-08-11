037b18bac27a5447858b354bc5af9ef100301d3e
kriztiank <kriztiank@gmail.com>
2023-08-11 09:28:35 +0200

    navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// components
+import Navbar from './components/Navbar'
+
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +13,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={rubik.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
