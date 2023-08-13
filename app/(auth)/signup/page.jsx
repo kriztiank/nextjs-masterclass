31af5d7aa63e7d2e9abc410e180109f2c4fd0d1f
kriztiank <kriztiank@gmail.com>
2023-08-13 15:01:21 +0200

    Signing up a New User

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,19 +1,46 @@
-'use client'
+"use client"
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 
-import AuthForm from '../AuthForm'
+// components
+import AuthForm from "../AuthForm"
 
 export default function Signup() {
+  const router = useRouter()
+  const [error, setError] = useState('')
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault()
+    setError('')
 
-    console.log('user signup', email, password)
+    // console.log('user signup', email, password)
+    const supabase = createClientComponentClient()
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`
+      }
+    })
+    if (error) {
+      setError(error.message)
+    }
+    if (!error) {
+      router.push('/verify')
+    } 
+
   }
 
   return (
     <main>
-      <h2 className='text-center'>Sign up</h2>
+      <h2 className="text-center">Sign up</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+
+      {error && (
+        <div className="error">{error}</div>
+      )}
     </main>
   )
 }
