f279f2eae72b3d37eb95ea2802754b795231652a
kriztiank <kriztiank@gmail.com>
2023-08-14 08:10:41 +0200

    login, logout & protecting routes

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,12 +1,32 @@
 'use client'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
+// components
 import AuthForm from '../AuthForm'
 
 export default function Login() {
-  const handleSubmit = async (e, email, password) => {
-    e.preventDefault()
+  const router = useRouter()
+  const [error, setError] = useState('')
 
-    console.log('user login', email, password)
+  const handleSubmit = async (e, email, password) => {
+    // console.log('user login', email, password)
+    e.preventDefault()
+    setError('')
+
+    const supabase = createClientComponentClient()
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    if (error) {
+      setError(error.message)
+    }
+    if (!error) {
+      router.push('/')
+    }
   }
 
   return (
@@ -14,6 +34,8 @@ export default function Login() {
       <h2 className='text-center'>Login</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+
+      {error && <div className='error'>{error}</div>}
     </main>
   )
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Logo from './dojo-logo.png'
+import LogoutButton from './LogoutButton'
 
-export default function Navbar() {
+export default function Navbar({ user }) {
   return (
     <nav>
-      <Image
-        src={Logo}
-        alt='Dojo Helpdesk logo'
-        width={70}
-        placeholder='blur'
-        quality={100}
-      />
+      <Image src={Logo} alt='Dojo Helpdesk logo' width={70} placeholder='blur' quality={100} />
       <h1>Dojo Helpdesk</h1>
-      <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
+      <Link href='/'>Dashboard</Link>
+      <Link href='/tickets' className='mr-auto'>
+        Tickets
+      </Link>
+      {/* get the current user */}
+      {user && <span>Hello, {user.email}</span>}
+      <LogoutButton />
     </nav>
   )
 }
