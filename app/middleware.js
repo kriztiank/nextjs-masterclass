8af3f1b2816f345510a95c0e68679b1186def316
kriztiank <kriztiank@gmail.com>
2023-08-14 08:18:57 +0200

    managing session with Middleware

---
+++ app/middleware.js
@@ -0,0 +1,11 @@
+// https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+
+export async function middleware(req) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  await supabase.auth.getSession()
+  return res
+}
