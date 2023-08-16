b0957d466f8eda08e0817ce24019cfe55f388f00
kriztiank <kriztiank@gmail.com>
2023-08-16 16:54:30 +0200

    Handling Server Errors

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -1,9 +1,9 @@
-"use server"
+'use server'
 
-import { createServerActionClient } from "@supabase/auth-helpers-nextjs"
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import { revalidatePath } from "next/cache"
-import { redirect } from "next/navigation"
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 export async function addTicket(formData) {
   const ticket = Object.fromEntries(formData)
@@ -11,14 +11,19 @@ export async function addTicket(formData) {
   const supabase = createServerActionClient({ cookies })
 
   // get current user session
-  const { data: { session } } = await supabase.auth.getSession()
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
 
   // insert the data
-  const { error } = await supabase.from('tickets')
-    .insert({
-      ...ticket,
-      user_email: session.user.email,
-    })
+  const { error } = await supabase.from('tickets').insert({
+    ...ticket,
+    user_email: session.user.email,
+  })
+
+  if (error) {
+    throw new Error('Could not add the new ticket.')
+  }
 
   revalidatePath('/tickets')
   redirect('/tickets')
