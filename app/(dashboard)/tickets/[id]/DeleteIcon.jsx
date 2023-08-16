223701f27d5982bf3b08b8c31ec36e65184054f9
kriztiank <kriztiank@gmail.com>
2023-08-16 17:33:27 +0200

    Delete server action with useTransition

--- app/(dashboard)/tickets/[id]/DeleteIcon.jsx
+++ app/(dashboard)/tickets/[id]/DeleteIcon.jsx
@@ -1,41 +1,75 @@
+// without server action
+// 'use client'
+// import { useState } from 'react'
+// import { useRouter } from 'next/navigation'
+
+// // icons & UI
+// import { TiDelete } from 'react-icons/ti'
+
+// export default function DeleteIcon({ id }) {
+//   const [isLoading, setIsLoading] = useState(false)
+//   const router = useRouter()
+
+//   const handleClick = async () => {
+//     setIsLoading(true)
+//     // console.log('deleting id - ', id)
+//     const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+//       method: 'DELETE',
+//     })
+//     const json = await res.json()
+
+//     if (json.error) {
+//       console.log(error)
+//       setIsLoading(false)
+//     }
+//     if (!json.error) {
+//       router.refresh()
+//       router.push('/tickets')
+//     }
+//   }
+
+//   return (
+//     <button className='btn-primary' onClick={handleClick} disabled={isLoading}>
+//       {isLoading && (
+//         <>
+//           <TiDelete />
+//           Deleting....
+//         </>
+//       )}
+//       {!isLoading && (
+//         <>
+//           <TiDelete />
+//           Delete Ticket
+//         </>
+//       )}
+//     </button>
+//   )
+// }
+
+// with server action
 'use client'
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useTransition } from 'react'
+import { deleteTicket } from '../actions'
 
 // icons & UI
 import { TiDelete } from 'react-icons/ti'
 
 export default function DeleteIcon({ id }) {
-  const [isLoading, setIsLoading] = useState(false)
-  const router = useRouter()
-
-  const handleClick = async () => {
-    setIsLoading(true)
-    // console.log('deleting id - ', id)
-    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
-      method: 'DELETE',
-    })
-    const json = await res.json()
-
-    if (json.error) {
-      console.log(error)
-      setIsLoading(false)
-    }
-    if (!json.error) {
-      router.refresh()
-      router.push('/tickets')
-    }
-  }
+  let [isPending, startTransition] = useTransition()
 
   return (
-    <button className='btn-primary' onClick={handleClick} disabled={isLoading}>
-      {isLoading && (
+    <button
+      className='btn-primary'
+      onClick={() => startTransition(() => deleteTicket(id))}
+      disabled={isPending}
+    >
+      {isPending && (
         <>
           <TiDelete />
           Deleting....
         </>
       )}
-      {!isLoading && (
+      {!isPending && (
         <>
           <TiDelete />
           Delete Ticket
--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -28,3 +28,19 @@ export async function addTicket(formData) {
   revalidatePath('/tickets')
   redirect('/tickets')
 }
+
+export async function deleteTicket(id) {
+  const supabase = createServerActionClient({ cookies })
+
+  // delete the data
+  const { error } = await supabase.from('tickets')
+    .delete()
+    .eq('id', id)
+
+  if (error) {
+    throw new Error('Could not delete the ticket.')
+  }
+
+  revalidatePath('/tickets')
+  redirect('/tickets')
+}
